43031eb1eba4aa4e714e8fb12575ef7a04ef6cd5
Benevolent Drone <benevolentdrone@gmail.com>
2023-02-05 08:20:00 +0200

    WIP Concurrent Managed Generic Circular Buffer

---
+++ HeresyMemory/src/Collections/Managed/Circular Buffers/ConsumerTicket.cs
@@ -0,0 +1,11 @@
+namespace HereticalSolutions.Collections.Managed
+{
+     public struct ConsumerTicket
+     {
+        public bool RequestSuccess = false;
+
+        public int Key = -1;
+
+        public int Index = -1;
+     }
+}
---
+++ HeresyMemory/src/Collections/Managed/Circular Buffers/ProducerTicket.cs
@@ -0,0 +1,11 @@
+namespace HereticalSolutions.Collections.Managed
+{
+     public struct ProducerTicket
+     {
+        public bool RequestSuccess = false;
+
+        public int Key = -1;
+
+        public int Index = -1;
+     }
+}
