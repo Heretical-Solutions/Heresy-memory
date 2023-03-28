05a8a9aab5ef455242955346be2049935417fb7e
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-28 17:50:13 +0300

    Squashed 'src/' changes from c131f4b..9244c6e
    9244c6e Added XML comments
    338b4d9 Updated interfaces and ensured decorator pools update push behaviour on element pop
    659ecaf Almost finished refactoring pool decorators
    7429c83 WIP refactoring
    
    git-subtree-dir: src
    git-subtree-split: 9244c6e65cdda061982496e0b242184a917b3d5a

--- Allocations/EAllocationStatus.cs
+++ Allocations/EAllocationStatus.cs
@@ -1,8 +1,18 @@
 namespace HereticalSolutions.Collections.Allocations
 {
+    /// <summary>
+    /// Specifies the status of the allocation
+    /// </summary>
     public enum EAllocationStatus
     {
+        /// <summary>
+        /// The element is free for use
+        /// </summary>
         FREE,
+        
+        /// <summary>
+        /// The element is in use
+        /// </summary>
         USED
     }
 }
--- Collections/Interfaces/IResizable.cs
+++ Collections/Interfaces/IResizable.cs
@@ -2,10 +2,19 @@ using HereticalSolutions.Collections.Allocations;
 
 namespace HereticalSolutions.Collections
 {
+	/// <summary>
+	/// Indicates that the collection can change its size
+	/// </summary>
 	public interface IResizable<T>
 	{
+		/// <summary>
+		/// The command for resizing allocations
+		/// </summary>
 		AllocationCommand<T> ResizeAllocationCommand { get; }
 
+		/// <summary>
+		/// Adds new elements to the collection
+		/// </summary>
 		void Resize();
 	}
 }
